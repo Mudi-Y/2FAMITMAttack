640f5f886d076a2b2cd72fbe870f136ec9cdab2a
Mudi Yang <mudi.yang@yale.edu>
2021-12-03 20:55:00 -0500

    created virtual enviroment, requirements file, and created RequestManager object in get_html module

--- get_html.py
+++ get_html.py
@@ -1,10 +1,16 @@
 import requests
 
-url = input('Webpage: ')
-html_output_name = input('Output file: ')
 
-req = requests.get(url)
+class RequestManager:
+    def __init__(self, home_url):
+        self.home = home_url
 
-with open(html_output_name, 'w') as f:
-    f.write(req.text)
-    f.close()
+    def getHtlm(self, site_url, output_file):
+        # url = input('Webpage: ')
+        # html_output_name = input('Output file: ')
+        output_file = str(output_file)
+        req = requests.get(site_url)
+
+        with open(output_file, 'w') as f:
+            f.write(req.text)
+            f.close()
